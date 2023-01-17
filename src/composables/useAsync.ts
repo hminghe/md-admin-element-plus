0761db56eaede83767c050cd83aba69a1c894311
hminghe <ming.he@qq.com>
2023-01-17 16:21:43 +0800

    useAsync: 重构

--- src/composables/useAsync.ts
+++ src/composables/useAsync.ts
@@ -1,11 +1,67 @@
-import { type AsyncStateOptions, type UseAsyncStateReturn } from '@vueuse/core'
+import type { Ref } from 'vue'
+import { ref } from 'vue'
 
-export function useAsync<Shallow extends boolean = true>(
-  promise: Promise<any> | ((...args: any[]) => Promise<any>),
-  options?: AsyncStateOptions<Shallow>,
-): UseAsyncStateReturn<void, Shallow> {
-  return useAsyncState(promise, undefined, {
-    immediate: false,
-    ...options,
+export interface UseAsyncReturn<Callback> {
+  isLoading: Ref<boolean>
+  execute: Callback
+}
+
+interface UseAsyncOptions {
+
+  immediate?: boolean
+
+  onError?: (e: unknown) => void
+
+  loadingService?: () => () => void
+
+  isLoading?: Ref<boolean>
+
+}
+
+export function useAsync<Data, Callback =((...args: any[]) => Promise<Data>)>(
+  callback: Callback,
+  options: UseAsyncOptions = {},
+): UseAsyncReturn<Callback> {
+  const isLoading = ref(false)
+
+  let loadingServiceClose: () => void
+  watch(isLoading, (isLoading) => {
+    if (options.isLoading) {
+      options.isLoading.value = isLoading
+    }
+
+    if (options.loadingService) {
+      if (isLoading) {
+        loadingServiceClose = options.loadingService()
+      } else if (loadingServiceClose) {
+        loadingServiceClose()
+      }
+    }
   })
+
+  async function execute(...args: any[]) {
+    try {
+      isLoading.value = true
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      const result = await callback(...args)
+      isLoading.value = false
+      return result
+    } catch (error) {
+      isLoading.value = false
+      if (options.onError) {
+        options.onError(error)
+      }
+      throw error
+    }
+  }
+
+  if (options.immediate) {
+    execute()
+  }
+
+  return {
+    isLoading,
+    execute: execute as unknown as Callback,
+  }
 }
